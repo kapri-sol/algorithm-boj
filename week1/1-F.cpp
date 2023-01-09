c2743e91ebb09d8d8aaf1ece000ad917c2e7971b
kapri <44130355+kapri-sol@users.noreply.github.com>
2023-01-10 08:28:27 +0900

    Solve 1-G

--- week1/1-F.cpp
+++ week1/1-F.cpp
@@ -9,11 +9,25 @@ char bigAlphabet[26];
 
 int main() {
     string input;
-    cin >> input;
+
+    getline(cin, input);
 
     for(int i = 0; i <= input.length(); i++) {
         char c = input[i];
-
+        if(c >= 'a' && c <= 'z') {
+            if(c + 13 <= 'z') {
+                input[i] = c + 13;
+            } else {
+                input[i] = ('a' - 1)  + (13 -('z' - c ));
+            }
+        } else if(c >= 'A' && c <= 'Z') {
+            if(c + 13 <= 'Z') {
+                input[i] = c + 13;
+            } else {
+                input[i] = ('A' - 1)  + (13 -('Z' - c ));
+            }
+        }
     }
+    cout << input;
     return 0;
 }
