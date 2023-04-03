f0e321f3fdc1faa5d0a43113158c8ee7da80e299
kapri.kim <ksm6902@gmail.com>
2023-04-03 18:27:41 +0900

    Solving 2-L

--- week2/2-K.cpp
+++ week2/2-K.cpp
@@ -1,11 +1,33 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-//int modTwo(int num) {
-//    if(num % 2) modTwo(num / 2)
-//}
-int main() {
+int T;
+unsigned long long N;
 
+int main() {
+    cin >> T;
+    vector<unsigned long long > v;
+
+    for(int t = 0; t < T; t++) {
+        cin >> N;
+        int acc2 = 0;
+        int acc5 = 0;
+
+        for(int i = 2; i <= N; i*=2) {
+            acc2 += N / i;
+        }
+
+        for(int i = 5; i <= N; i*=5) {
+            acc5 += N / i;
+        }
+
+        v.push_back(min(acc2, acc5));
+    }
+
+    for(int i = 0; i < v.size(); i++) {
+        cout << v[i] << "\n";
+    }
     return 0;
 }
---
+++ week2/2-L.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int N;
+
+int main() {
+    cin >> N;
+    int score1 = 0;
+    int score2 = 0;
+
+    for(int i = 0; i < N; i++) {
+        
+    }
+
+    return 0;
+}
