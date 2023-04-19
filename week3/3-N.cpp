36c0aefe41182b94bfa14443526cffe6c02b19d5
kapri <ksm6902@gmail.com>
2023-04-20 08:48:51 +0900

    Solving 3-N

--- week3/3-N.cpp
+++ week3/3-N.cpp
@@ -1,7 +1,40 @@
 #include <iostream>
+#include <cmath>
+#include <algorithm>
 
 using namespace std;
 
+int K, nums[1024];
+vector<int> tree[10];
+
+void search(int start, int end, int level) {
+    int mid = (start + end) / 2;
+    mid += (start + end) % 2;
+
+    tree[level].push_back(nums[mid]);
+
+    if(mid == start || mid == end) return;
+
+    search(start, mid-1, level + 1);
+    search(mid, end, level + 1);
+}
+
+void print() {
+    for(int i = 0; i < K; i++) {
+        for(int j = 0; j < tree[i].size(); j++) {
+            cout << tree[i][j] << " ";
+        }
+        cout << "\n";
+    }
+}
+
 int main() {
+    cin >> K;
+
+    for(int i = 0; i < pow(2, K) - 1; i++)
+        cin >> nums[i];
+
+    search(0, pow(2, K)-2, 0);
+    print();
     return 0;
 }
